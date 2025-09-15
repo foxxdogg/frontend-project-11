0d1a5b90ebfe6c9ac78cd7d378d8aa74a1b5004e
foxdog <alexkolymago@gmail.com>
2025-09-15 21:01:07 +0300

    step2: refactor3

--- src/main.js
+++ src/main.js
@@ -3,42 +3,44 @@ import * as yup from 'yup'
 import { initView } from './view.js'
 import state from './state.js'
 
-const { watchedState, input, form } = initView()
-form.addEventListener('submit', (e) => {
-  e.preventDefault()
-  validateForm(input.value, state.rssForm.feeds)
-})
+document.addEventListener('DOMContentLoaded', () => {
+  const { watchedState, input, form } = initView()
+  form.addEventListener('submit', (e) => {
+    e.preventDefault()
+    validateForm(input.value, state.rssForm.feeds)
+  })
 
-function validateForm(link, feeds) {
-  yup
-    .string()
-    .url()
-    .required()
-    .test(
-      'unique',
-      'rss-link must be unique',
-      value =>
-        new Promise((resolve) => {
-          if (!value) {
-            resolve(false)
-          }
-          else if (feeds.includes(value)) {
-            resolve(false)
-          }
-          else {
-            resolve(true)
-          }
-        }),
-    )
-    .validate(link)
-    .then(() => {
-      watchedState.rssForm.status = 'valid'
-      watchedState.rssForm.feeds.push(link)
-    })
-    .catch((e) => {
-      watchedState.rssForm.status = 'invalid'
-      console.log(e)
-    })
-}
+  function validateForm(link, feeds) {
+    yup
+      .string()
+      .url()
+      .required()
+      .test(
+        'unique',
+        'rss-link must be unique',
+        value =>
+          new Promise((resolve) => {
+            if (!value) {
+              resolve(false)
+            }
+            else if (feeds.includes(value)) {
+              resolve(false)
+            }
+            else {
+              resolve(true)
+            }
+          }),
+      )
+      .validate(link)
+      .then(() => {
+        watchedState.rssForm.status = 'valid'
+        watchedState.rssForm.feeds.push(link)
+      })
+      .catch((e) => {
+        watchedState.rssForm.status = 'invalid'
+        console.log(e)
+      })
+  }
+})
 
 export { state }
